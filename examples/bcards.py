b6dca0e136015290af0a1b048962b973351bbe77
Ken Seehart <ken@seehart.com>
2020-05-01 01:39:10 -0700

    Update bcards.py
    compatibility with hiding unbound_field layer

--- examples/bcards.py
+++ examples/bcards.py
@@ -49,7 +49,7 @@ class BCardsTest(unittest.TestCase):
 
     def test_deck(self):
         
-        deck = card[52]('deck')(range(52))
+        deck = card[52](range(52))
         self.assertEqual(deck, ['2C', '2H', '2D', '2S', '3C', '3H', '3D', '3S', '4C', '4H', '4D', '4S', '5C',
                                 '5H', '5D', '5S', '6C', '6H', '6D', '6S', '7C', '7H', '7D', '7S', '8C', '8H',
                                 '8D', '8S', '9C', '9H', '9D', '9S', 'TC', 'TH', 'TD', 'TS', 'JC', 'JH', 'JD',
